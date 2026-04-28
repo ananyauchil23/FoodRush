1578b9d3762a4b09e8df2644a0c61f834c470309
Ananya <ananyauchil06@gmail.com>
2026-04-28 14:21:31 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    process.env.FRONTEND_URL,         
+    "https://food-rush-wine.vercel.app/",         
   ],
   credentials: true,
 }));
