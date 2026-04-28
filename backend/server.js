859a29b069fa22fa720f763b018c9150049900b7
Ananya <ananyauchil06@gmail.com>
2026-04-28 14:33:01 +0530

    cors configuration

--- backend/server.js
+++ backend/server.js
@@ -27,11 +27,7 @@ const app = express();
 //   credentials: true,
 // }));
 app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    process.env.FRONTEND_URL,         
-  ],
-  credentials: true,
+  origin: "*"
 }));
 
 
